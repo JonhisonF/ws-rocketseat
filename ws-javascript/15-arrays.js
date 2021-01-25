a9b74c5f7c829eabd6816f85e7695f7619ee7ca4
JonhisonF <jonhisonfilho47@gmail.com>
2021-01-24 22:48:22 -0300

    Adding 'praticando e avançando'

---
+++ ws-javascript/15-arrays.js
@@ -0,0 +1,17 @@
+// Array
+
+// é bom ter padrão, a mistura de objetos, strings, e number entre outras coisas
+// não é uma boa ideia
+const animals = [
+    'Lion',
+    'Monkey',
+    {
+        name: 'Cat',
+        age: 3
+    }
+];
+
+// Acessar valores dentro do Array
+// acessar a propriedade name daquele objeto
+// console.log(animals[2].name);
+console.log(animals[2]);
